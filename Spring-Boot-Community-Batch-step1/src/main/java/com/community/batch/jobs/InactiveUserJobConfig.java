f461015a51820b6129f618221db14e944bf8cf35
kgc0120 <senru0508@gmail.com>
2021-05-23 23:38:56 +0900

    batch clear

--- Spring-Boot-Community-Batch-step1/src/main/java/com/community/batch/jobs/InactiveUserJobConfig.java
+++ Spring-Boot-Community-Batch-step1/src/main/java/com/community/batch/jobs/InactiveUserJobConfig.java
@@ -2,24 +2,34 @@ package com.community.batch.jobs;
 
 import com.community.batch.domain.User;
 import com.community.batch.domain.enums.UserStatus;
-import com.community.batch.jobs.readers.QueueItemReader;
+import com.community.batch.jobs.inactice.listener.InacticeJobListener;
+import com.community.batch.jobs.inactice.listener.InactiveStepListener;
 import com.community.batch.repository.UserRepository;
+import lombok.AllArgsConstructor;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.job.builder.FlowBuilder;
+import org.springframework.batch.core.job.flow.Flow;
+import org.springframework.batch.core.job.flow.FlowExecutionStatus;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JpaItemWriter;
 import org.springframework.batch.item.support.ListItemReader;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.core.task.TaskExecutor;
 
+import javax.persistence.EntityManagerFactory;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
 import java.util.List;
-
-import lombok.AllArgsConstructor;
 
 /**
  * Created by KimYJ on 2018-03-07.
@@ -29,31 +39,81 @@ import lombok.AllArgsConstructor;
 public class InactiveUserJobConfig {
 
     private UserRepository userRepository;
+    private final static int CHUNK_SIZE = 5;
+    private final EntityManagerFactory entityManagerFactory;
 
     @Bean
-    public Job inactiveUserJob(JobBuilderFactory jobBuilderFactory, Step inactiveJobStep) {
+    public Job inactiveUserJob(JobBuilderFactory jobBuilderFactory,
+                               InacticeJobListener inacticeJobListener, Flow inactiveJobFlow) {
         return jobBuilderFactory.get("inactiveUserJob")
                 .preventRestart()
-                .start(inactiveJobStep)
+                .listener(inacticeJobListener)
+                .start(inactiveJobFlow)
+                .end()
                 .build();
     }
 
     @Bean
-    public Step inactiveJobStep(StepBuilderFactory stepBuilderFactory) {
+    public Flow inactiveJobFlow(Step inactiveJobStep) {
+        FlowBuilder<Flow> flowBuilder = new FlowBuilder<>("inactiveJobFlow");
+        return flowBuilder
+                .start(new InactiveJobExecutionDecider())
+                .on(FlowExecutionStatus.FAILED.getName()).end()
+                .on(FlowExecutionStatus.COMPLETED.getName()).to(inactiveJobStep)
+                .end();
+    }
+
+
+    @Bean
+    public Step inactiveJobStep(StepBuilderFactory stepBuilderFactory,
+                                ListItemReader<User> inactiveUserReader,
+                                InactiveStepListener inactiveStepListener,
+                                TaskExecutor taskExecutor) {
         return stepBuilderFactory.get("inactiveUserStep")
                 .<User, User> chunk(10)
-                .reader(inactiveUserReader())
+                .reader(inactiveUserReader)
                 .processor(inactiveUserProcessor())
                 .writer(inactiveUserWriter())
+                .listener(inactiveStepListener)
+                .taskExecutor(taskExecutor)
+                .throttleLimit(2)
                 .build();
+    }
+
+    @Bean
+    public TaskExecutor taskExecutor() {
+        return new SimpleAsyncTaskExecutor("Batch_Task");
     }
 
     @Bean
     @StepScope
-    public ListItemReader<User> inactiveUserReader() {
-        List<User> oldUsers = userRepository.findByUpdatedDateBeforeAndStatusEquals(LocalDateTime.now().minusYears(1), UserStatus.ACTIVE);
+    public ListItemReader<User> inactiveUserReader(@Value("#{jobParameters[nowDate]}") Date nowDate, UserRepository userRepository) {
+        LocalDateTime now = LocalDateTime.ofInstant(nowDate.toInstant(), ZoneId.systemDefault());
+        List<User> oldUsers = userRepository.findByUpdatedDateBeforeAndStatusEquals(now.minusYears(1), UserStatus.ACTIVE);
         return new ListItemReader<>(oldUsers);
     }
+
+//    @Bean(destroyMethod="")
+//    @StepScope
+//    public JpaPagingItemReader<User> inactiveUserJpaReader() {
+//
+//        JpaPagingItemReader<User> jpaPagingItemReader = new JpaPagingItemReader() {
+//            @Override
+//            public int getPage() {
+//              return  0;
+//            }
+//        };
+//        jpaPagingItemReader.setQueryString("select u from User as u where u.createdDate < :createdDate and u.status = :status");
+//        Map<String, Object> map = new HashMap<>();
+//        LocalDateTime now = LocalDateTime.now();
+//        map.put("createdDate", now.minusYears(1));
+//        map.put("status", UserStatus.ACTIVE);
+//
+//        jpaPagingItemReader.setParameterValues(map);
+//        jpaPagingItemReader.setEntityManagerFactory(entityManagerFactory); // 트랜잭션을 관리해줄 entityManagerFactory
+//        jpaPagingItemReader.setPageSize(CHUNK_SIZE);
+//        return jpaPagingItemReader;
+//    }
 
     public ItemProcessor<User, User> inactiveUserProcessor() {
         return User::setInactive;
@@ -68,6 +128,8 @@ public class InactiveUserJobConfig {
     }
 
     public ItemWriter<User> inactiveUserWriter() {
-        return ((List<? extends User> users) -> userRepository.saveAll(users));
+        JpaItemWriter<User> jpaItemWriter = new JpaItemWriter<>();
+        jpaItemWriter.setEntityManagerFactory(entityManagerFactory);
+        return jpaItemWriter;
     }
 }
--- Spring-Boot-Community-Batch-step1/src/test/java/com/community/batch/InactiveUserJobTest.java
+++ Spring-Boot-Community-Batch-step1/src/test/java/com/community/batch/InactiveUserJobTest.java
@@ -6,17 +6,22 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.test.JobLauncherTestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.time.LocalDateTime;
+import java.util.Date;
 
 import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 public class InactiveUserJobTest {
 
 	@Autowired
@@ -27,7 +32,10 @@ public class InactiveUserJobTest {
 
 	@Test
 	public void 휴면_회원_전환_테스트() throws Exception {
-		JobExecution jobExecution = jobLauncherTestUtils.launchJob();
+		Date nowDate = new Date();
+
+		JobExecution jobExecution = jobLauncherTestUtils.launchJob(
+				new JobParametersBuilder().addDate("nowDate", nowDate).toJobParameters());
 
 		assertEquals(BatchStatus.COMPLETED, jobExecution.getStatus());
 		assertEquals(0, userRepository.findByUpdatedDateBeforeAndStatusEquals(LocalDateTime.now().minusYears(1), UserStatus.ACTIVE).size());
